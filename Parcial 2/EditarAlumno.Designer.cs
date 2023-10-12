4876998e9eb9f1801c325ba4512f11764ec6bd8f
sebas <sebas@EGGDESK>
2023-10-12 16:39:09 -0600

    Se agrego la logica para poder editar estudiantes

--- Parcial 2/EditarAlumno.Designer.cs
+++ Parcial 2/EditarAlumno.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.Correolabel = new System.Windows.Forms.Label();
+            this.Telefonolabel = new System.Windows.Forms.Label();
+            this.Direccionlabel = new System.Windows.Forms.Label();
+            this.Fechalabel = new System.Windows.Forms.Label();
+            this.Apellidolabel = new System.Windows.Forms.Label();
+            this.Nombrelabel = new System.Windows.Forms.Label();
+            this.Editar = new System.Windows.Forms.Button();
+            this.Correotext = new System.Windows.Forms.TextBox();
+            this.Numerotext = new System.Windows.Forms.TextBox();
+            this.Fechanacimiento = new System.Windows.Forms.DateTimePicker();
+            this.Direcciontext = new System.Windows.Forms.TextBox();
+            this.Apellidotext = new System.Windows.Forms.TextBox();
+            this.Nombretext = new System.Windows.Forms.TextBox();
+            this.SeleccionarEstudiante = new System.Windows.Forms.DomainUpDown();
+            this.Bienvenidalabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -43,21 +59,213 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Correolabel
+            // 
+            this.Correolabel.AutoSize = true;
+            this.Correolabel.BackColor = System.Drawing.Color.White;
+            this.Correolabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Correolabel.Location = new System.Drawing.Point(545, 228);
+            this.Correolabel.Name = "Correolabel";
+            this.Correolabel.Size = new System.Drawing.Size(65, 22);
+            this.Correolabel.TabIndex = 36;
+            this.Correolabel.Text = "Correo";
+            // 
+            // Telefonolabel
+            // 
+            this.Telefonolabel.AutoSize = true;
+            this.Telefonolabel.BackColor = System.Drawing.Color.White;
+            this.Telefonolabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Telefonolabel.Location = new System.Drawing.Point(545, 159);
+            this.Telefonolabel.Name = "Telefonolabel";
+            this.Telefonolabel.Size = new System.Drawing.Size(81, 22);
+            this.Telefonolabel.TabIndex = 35;
+            this.Telefonolabel.Text = "Telefono";
+            // 
+            // Direccionlabel
+            // 
+            this.Direccionlabel.AutoSize = true;
+            this.Direccionlabel.BackColor = System.Drawing.Color.White;
+            this.Direccionlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Direccionlabel.Location = new System.Drawing.Point(547, 294);
+            this.Direccionlabel.Name = "Direccionlabel";
+            this.Direccionlabel.Size = new System.Drawing.Size(85, 22);
+            this.Direccionlabel.TabIndex = 33;
+            this.Direccionlabel.Text = "Direccion";
+            // 
+            // Fechalabel
+            // 
+            this.Fechalabel.AutoSize = true;
+            this.Fechalabel.BackColor = System.Drawing.Color.White;
+            this.Fechalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fechalabel.Location = new System.Drawing.Point(256, 293);
+            this.Fechalabel.Name = "Fechalabel";
+            this.Fechalabel.Size = new System.Drawing.Size(154, 22);
+            this.Fechalabel.TabIndex = 32;
+            this.Fechalabel.Text = "Fecha Nacimiento";
+            // 
+            // Apellidolabel
+            // 
+            this.Apellidolabel.AutoSize = true;
+            this.Apellidolabel.BackColor = System.Drawing.Color.White;
+            this.Apellidolabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Apellidolabel.Location = new System.Drawing.Point(255, 228);
+            this.Apellidolabel.Name = "Apellidolabel";
+            this.Apellidolabel.Size = new System.Drawing.Size(74, 22);
+            this.Apellidolabel.TabIndex = 31;
+            this.Apellidolabel.Text = "Apellido";
+            // 
+            // Nombrelabel
+            // 
+            this.Nombrelabel.AutoSize = true;
+            this.Nombrelabel.BackColor = System.Drawing.Color.White;
+            this.Nombrelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nombrelabel.Location = new System.Drawing.Point(255, 159);
+            this.Nombrelabel.Name = "Nombrelabel";
+            this.Nombrelabel.Size = new System.Drawing.Size(73, 22);
+            this.Nombrelabel.TabIndex = 30;
+            this.Nombrelabel.Text = "Nombre";
+            // 
+            // Editar
+            // 
+            this.Editar.BackColor = System.Drawing.Color.Yellow;
+            this.Editar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Editar.Location = new System.Drawing.Point(351, 384);
+            this.Editar.Name = "Editar";
+            this.Editar.Size = new System.Drawing.Size(305, 54);
+            this.Editar.TabIndex = 29;
+            this.Editar.Text = "Guardar Cambios";
+            this.Editar.UseVisualStyleBackColor = false;
+            this.Editar.Click += new System.EventHandler(this.Editar_Click);
+            // 
+            // Correotext
+            // 
+            this.Correotext.BackColor = System.Drawing.SystemColors.Info;
+            this.Correotext.Location = new System.Drawing.Point(546, 259);
+            this.Correotext.Name = "Correotext";
+            this.Correotext.Size = new System.Drawing.Size(235, 22);
+            this.Correotext.TabIndex = 28;
+            // 
+            // Numerotext
+            // 
+            this.Numerotext.BackColor = System.Drawing.SystemColors.Info;
+            this.Numerotext.Location = new System.Drawing.Point(546, 192);
+            this.Numerotext.Name = "Numerotext";
+            this.Numerotext.Size = new System.Drawing.Size(235, 22);
+            this.Numerotext.TabIndex = 27;
+            this.Numerotext.TextChanged += new System.EventHandler(this.Numerotext_TextChanged);
+            // 
+            // Fechanacimiento
+            // 
+            this.Fechanacimiento.CalendarMonthBackground = System.Drawing.SystemColors.Info;
+            this.Fechanacimiento.Location = new System.Drawing.Point(255, 327);
+            this.Fechanacimiento.Name = "Fechanacimiento";
+            this.Fechanacimiento.Size = new System.Drawing.Size(235, 22);
+            this.Fechanacimiento.TabIndex = 26;
+            // 
+            // Direcciontext
+            // 
+            this.Direcciontext.BackColor = System.Drawing.SystemColors.Info;
+            this.Direcciontext.Location = new System.Drawing.Point(546, 329);
+            this.Direcciontext.Name = "Direcciontext";
+            this.Direcciontext.Size = new System.Drawing.Size(235, 22);
+            this.Direcciontext.TabIndex = 24;
+            // 
+            // Apellidotext
+            // 
+            this.Apellidotext.BackColor = System.Drawing.SystemColors.Info;
+            this.Apellidotext.Location = new System.Drawing.Point(255, 259);
+            this.Apellidotext.Name = "Apellidotext";
+            this.Apellidotext.Size = new System.Drawing.Size(235, 22);
+            this.Apellidotext.TabIndex = 23;
+            // 
+            // Nombretext
+            // 
+            this.Nombretext.BackColor = System.Drawing.SystemColors.Info;
+            this.Nombretext.Location = new System.Drawing.Point(255, 194);
+            this.Nombretext.Name = "Nombretext";
+            this.Nombretext.Size = new System.Drawing.Size(235, 22);
+            this.Nombretext.TabIndex = 22;
+            // 
+            // SeleccionarEstudiante
+            // 
+            this.SeleccionarEstudiante.Location = new System.Drawing.Point(38, 159);
+            this.SeleccionarEstudiante.Name = "SeleccionarEstudiante";
+            this.SeleccionarEstudiante.Size = new System.Drawing.Size(145, 22);
+            this.SeleccionarEstudiante.TabIndex = 37;
+            this.SeleccionarEstudiante.Text = "Estudiantes";
+            this.SeleccionarEstudiante.SelectedItemChanged += new System.EventHandler(this.SeleccionarEstudiante_SelectedItemChanged);
+            // 
+            // Bienvenidalabel
+            // 
+            this.Bienvenidalabel.AutoSize = true;
+            this.Bienvenidalabel.BackColor = System.Drawing.Color.White;
+            this.Bienvenidalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bienvenidalabel.Location = new System.Drawing.Point(15, 14);
+            this.Bienvenidalabel.Name = "Bienvenidalabel";
+            this.Bienvenidalabel.Size = new System.Drawing.Size(402, 54);
+            this.Bienvenidalabel.TabIndex = 38;
+            this.Bienvenidalabel.Text = "Editar Estudiantes";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 36);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Seleccione el nombre\r\ndel estudiante que desea editar";
+            // 
             // EditarAlumno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Bienvenidalabel);
+            this.Controls.Add(this.SeleccionarEstudiante);
+            this.Controls.Add(this.Correolabel);
+            this.Controls.Add(this.Telefonolabel);
+            this.Controls.Add(this.Direccionlabel);
+            this.Controls.Add(this.Fechalabel);
+            this.Controls.Add(this.Apellidolabel);
+            this.Controls.Add(this.Nombrelabel);
+            this.Controls.Add(this.Editar);
+            this.Controls.Add(this.Correotext);
+            this.Controls.Add(this.Numerotext);
+            this.Controls.Add(this.Fechanacimiento);
+            this.Controls.Add(this.Direcciontext);
+            this.Controls.Add(this.Apellidotext);
+            this.Controls.Add(this.Nombretext);
             this.Controls.Add(this.button1);
             this.Name = "EditarAlumno";
             this.Text = "EditarAlumno";
             this.Load += new System.EventHandler(this.EditarAlumno_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Correolabel;
+        private System.Windows.Forms.Label Telefonolabel;
+        private System.Windows.Forms.Label Direccionlabel;
+        private System.Windows.Forms.Label Fechalabel;
+        private System.Windows.Forms.Label Apellidolabel;
+        private System.Windows.Forms.Label Nombrelabel;
+        private System.Windows.Forms.Button Editar;
+        private System.Windows.Forms.TextBox Correotext;
+        private System.Windows.Forms.TextBox Numerotext;
+        private System.Windows.Forms.DateTimePicker Fechanacimiento;
+        private System.Windows.Forms.TextBox Direcciontext;
+        private System.Windows.Forms.TextBox Apellidotext;
+        private System.Windows.Forms.TextBox Nombretext;
+        private System.Windows.Forms.DomainUpDown SeleccionarEstudiante;
+        private System.Windows.Forms.Label Bienvenidalabel;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Parcial 2/EditarAlumno.cs
+++ Parcial 2/EditarAlumno.cs
@@ -12,11 +12,18 @@ namespace Parcial_2
 {
     public partial class EditarAlumno : Form
     {
+        List<string> nombresEstudiantes;
         Logistica logistica = new Logistica();
         public EditarAlumno(Logistica logistica)
         {
             InitializeComponent();
             this.logistica = logistica;
+            nombresEstudiantes = logistica.listaEstudiantes.Select(estudiante => estudiante.Nombre).ToList();
+            ConfigureDomainUpDown();
+        }
+        private void ConfigureDomainUpDown()
+        {
+            SeleccionarEstudiante.Items.AddRange(nombresEstudiantes.ToArray());
         }
 
         private void EditarAlumno_Load(object sender, EventArgs e)
@@ -29,6 +36,68 @@ namespace Parcial_2
             Gestion_Alumnos gestionalumnos = new Gestion_Alumnos(logistica);
             this.Hide();
             gestionalumnos.Show();
+        }
+
+        private void SeleccionarEstudiante_SelectedItemChanged(object sender, EventArgs e)
+        {
+            textbox();
+        }
+
+        private void Editar_Click(object sender, EventArgs e)
+        {
+            string estudianteSeleccionado = SeleccionarEstudiante.Text;
+            if (!string.IsNullOrEmpty(estudianteSeleccionado))
+            {
+                Estudiante estudianteEncontrado = logistica.listaEstudiantes.FirstOrDefault(est => est.Nombre == estudianteSeleccionado);
+                if (estudianteEncontrado != null)
+                {
+
+                    estudianteEncontrado.Nombre = Nombretext.Text;
+                    estudianteEncontrado.Apellido = Apellidotext.Text;
+                    estudianteEncontrado.FechaNacimiento = Fechanacimiento.Value;
+                    estudianteEncontrado.CorreoElectronico = Correotext.Text;
+                    estudianteEncontrado.Direccion = Direcciontext.Text;
+                    int numeroCelular;
+                    if (int.TryParse(Numerotext.Text, out numeroCelular))
+                    {
+                        estudianteEncontrado.NumeroCelular = numeroCelular;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Número de celular no válido. No se realizó la actualización.");
+                        
+                    }
+                    MessageBox.Show("Datos actualizados exitosamente");
+                }
+                else
+                {
+                    MessageBox.Show($"Estudiante no encontrado: {estudianteSeleccionado}");
+                }
+            }
+        }
+
+        private void textbox()
+        {
+            string estudianteSeleccionado = SeleccionarEstudiante.Text;
+            if (!string.IsNullOrEmpty(estudianteSeleccionado))
+            {
+                Estudiante estudianteEncontrado = logistica.listaEstudiantes.FirstOrDefault(est => est.Nombre == estudianteSeleccionado);
+                this.Nombretext.Text = estudianteEncontrado.Nombre;
+                this.Apellidotext.Text = estudianteEncontrado.Apellido;
+                this.Fechanacimiento.Value = estudianteEncontrado.FechaNacimiento;
+                this.Numerotext.Text = estudianteEncontrado.NumeroCelular.ToString();
+                this.Correotext.Text = estudianteEncontrado.CorreoElectronico;
+                this.Direcciontext.Text = estudianteEncontrado.Direccion;
+            }
+        }
+
+        private void Numerotext_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Numerotext.Text) && !int.TryParse(Numerotext.Text, out _))
+            {
+                MessageBox.Show("Solo se permiten números enteros en este campo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Numerotext.Text = string.Empty;
+            }
         }
     }
 }
